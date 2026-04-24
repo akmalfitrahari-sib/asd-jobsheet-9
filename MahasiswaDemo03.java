7c2ffb0d0b1c5944e946561e163df0ca53f83dce
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-04-24 17:32:44 +0700

    MhsDemo menu nilai terbawah

--- MahasiswaDemo03.java
+++ MahasiswaDemo03.java
@@ -11,6 +11,7 @@ public class MahasiswaDemo03 {
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Melihat Tugas Terbawah");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
@@ -46,6 +47,12 @@ public class MahasiswaDemo03 {
                     System.out.println("Daftar semua tugas");
                     System.out.println("Nama\tNIM\tKelas");
                     stack.print();
+                    break;
+                case 5:
+                    Mahasiswa03 bawah = stack.peekBottom();
+                    if (bawah != null) {
+                        System.out.println("Tugas pertama dikumpulkan oleh " + bawah.nama);
+                    }
                     break;
                 default:
                     System.out.println("Pilihan tidak valid.");
