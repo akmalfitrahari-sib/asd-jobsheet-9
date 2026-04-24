8adb2bdcc7848a88b29bea1114f758e763611258
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-04-24 17:33:04 +0700

    StackTugas method peekBottom

--- StackTugasMahasiswa03.java
+++ StackTugasMahasiswa03.java
@@ -54,6 +54,14 @@ public class StackTugasMahasiswa03 {
         }
     }
 
+    public Mahasiswa03 peekBottom(){
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong!");
+            return null;
+        }
+    }
     public void print(){
         for(int i = top; i >= 0; i--){
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
